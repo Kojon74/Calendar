bed5e821c07ffbe8e4842aad446895322116f610
Ken Johnson <dragonkenkj@gmail.com>
2023-01-26 19:07:01 -0800

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calendar
+A React Native Calendar Component that immitates the iOS Calendar App.
---
+++ src/screens/DayViewScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const DayViewScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>DayViewScreen</Text>
+    </View>
+  );
+};
+
+export default DayViewScreen;
+
+const styles = StyleSheet.create({});
